0e672f82588db434795203cd51b8e31fdd885132
Damyan Petev <damyanpetev@users.noreply.github.com>
2024-05-20 18:37:20 +0300

    ng g c comp2 --project my-lib

---
+++ projects/my-lib/src/lib/comp2/comp2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-comp2',
+  standalone: true,
+  imports: [],
+  templateUrl: './comp2.component.html',
+  styleUrl: './comp2.component.css'
+})
+export class Comp2Component {
+
+}
--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -4,3 +4,4 @@
 
 export * from './lib/my-lib.service';
 export * from './lib/my-lib.component';
+export * from './lib/comp2/comp2.component';
